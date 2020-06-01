3559523f0fc793e2aaafe1a19cce9a2f90b78a38
Brian de Buiteach <debuiteb@tcd.ie>
2020-06-01 18:43:01 +0100

    prints

--- pre_commit_hooks/detect_verbose_curl.py
+++ pre_commit_hooks/detect_verbose_curl.py
@@ -15,10 +15,9 @@ def _get_file_verbose_occurrences(filename: str) -> int:
             if CURL_VERBOSE_PATTERN.search(line):
                 print(
                     f'Talkative/Verbose cURL command found:'
-                    f'{repr(filename)}: {repr(i)} : {line.decode("utf-8")}',
+                    f'{repr(filename)}:{repr(i)}:{line.decode("utf-8")}',
                     end='',
                 )
-                print(f'{repr(line)}  ------{i}')
                 file_verbose_occurrences += 1
     return file_verbose_occurrences
 
